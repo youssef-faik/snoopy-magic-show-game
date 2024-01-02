80111bcced5b97ded59a66e5053b45bf0a1333a8
Youssef Faik <faik.yusef@gmail.com>
2024-01-02 14:50:17 +0100

    move function declarations to separate header file and their implementations to a source file

---
+++ include/game_structs.h
@@ -0,0 +1,14 @@
+//
+// Created by Youssef Faik on 1/2/2024.
+//
+
+#ifndef SNOOPY_GAME_GAME_STRUCTS_H
+#define SNOOPY_GAME_GAME_STRUCTS_H
+
+typedef struct {
+    int x;
+    int y;
+    char newValue;
+} Update;
+
+#endif //SNOOPY_GAME_GAME_STRUCTS_H
